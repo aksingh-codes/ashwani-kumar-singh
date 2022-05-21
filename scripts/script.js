9e72dba47eda25d503c754578619de7dd4bbe4dc
aksingh <dev.ak.singh@outlook.com>
2022-05-21 20:39:51 +0530

    saved darkmode in localstorage

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,40 @@
+let theme = localStorage.getItem("theme") || "light";
 const themeSelector = document.querySelector(".theme-selector");
 
+const body = document.body;
+
+if (theme === "light") {
+  switchToLight();
+} else {
+  switchToDark();
+}
+saveTheme();
+
 themeSelector.addEventListener("click", () => {
-  const body = document.body;
-  console.log(body);
-  body.classList.toggle("dark-theme");
+  if (theme === "light") {
+    switchToDark();
+  } else {
+    switchToLight();
+  }
+  saveTheme();
 });
+
+function switchToLight() {
+  theme = "light";
+  body.classList.remove("dark-theme");
+  themeSelector.querySelector(".fa-sun").classList.remove("hidden");
+  themeSelector.querySelector(".fa-moon").classList.add("hidden");
+  themeSelector.querySelector("p").innerText = "Light Mode";
+}
+
+function switchToDark() {
+  theme = "dark";
+  body.classList.add("dark-theme");
+  themeSelector.querySelector(".fa-sun").classList.add("hidden");
+  themeSelector.querySelector(".fa-moon").classList.remove("hidden");
+  themeSelector.querySelector("p").innerText = "Dark Mode";
+}
+
+function saveTheme() {
+  localStorage.setItem("theme", theme);
+}
