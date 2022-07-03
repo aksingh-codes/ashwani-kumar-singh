5924ca93a7a00c06aafbdf503a8f842f939f2994
Ashwani Kumar Singh <code.ak.singh@gmail.com>
2022-07-03 18:26:33 +0530

    added social links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,36 @@
 import React from "react";
-import { FaGithub, FaTwitter, FaLinkedin, FaDiscord, FaEnvelope } from "react-icons/fa"
-import { SiGatsby } from "react-icons/si"
+import {
+  FaGithub,
+  FaTwitter,
+  FaLinkedin,
+  FaDiscord,
+  FaEnvelope,
+} from "react-icons/fa";
+import { SiGatsby } from "react-icons/si";
 
 const Footer = () => {
   return (
     <footer class="container">
-      <p>Built with <SiGatsby /> and Vanilla CSS</p>
+      <p>
+        Built with <SiGatsby /> and Vanilla CSS
+      </p>
       <p>by aksingh</p>
       <div class="footer-container">
-        <FaGithub />
-        <FaTwitter />
-        <FaLinkedin />
-        <FaDiscord />
-        <FaEnvelope />
+        <a href="https://github.com/aksingh-codes">
+          <FaGithub />
+        </a>
+        <a href="https://twitter.com/aksingh_codes">
+          <FaTwitter />
+        </a>
+        <a href="https://www.linkedin.com/in/aksingh-codes/">
+          <FaLinkedin />
+        </a>
+        {/* <a href="">
+          <FaDiscord />
+        </a> */}
+        <a href="mailto://dev.ashwani.kumar.singh@gmail.com">
+          <FaEnvelope />
+        </a>
       </div>
     </footer>
   );
