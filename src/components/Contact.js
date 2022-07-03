3ca44c62597eabdb132625534db3dc8f9255db25
Ashwani Kumar Singh <code.ak.singh@gmail.com>
2022-07-03 18:11:39 +0530

    feat: completed contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,15 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import validator from "email-validator";
+import emailjs from "@emailjs/browser";
+import { toast } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
 
 const Contact = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const [loading, setLoading] = useState(false);
+
+  const onSubmit = async (data) => {
+    setLoading(true);
+
+    const templateParams = {
+      from_name: `${data.firstName} ${data.lastName}`,
+      to_name: "Ashwani",
+      email: data.email,
+      message: data.messageBox,
+    };
+
+    const response = await toast.promise(
+      emailjs.send(
+        process.env.GATSBY_API_SERVICE_ID,
+        process.env.GATSBY_API_TEMPLATE_ID,
+        templateParams,
+        process.env.GATSBY_API_PUBLIC_KEY
+      ),
+      {
+        pending: "Sending your pretty message please wait ☺️",
+        success: "Thanks for contacting me, I will get back to you soon 🙋‍♂️",
+        error:
+          "Couldn't send message, please check your internet connection 🤯",
+      }
+    );
+
+    setLoading(false);
+  };
+
   return (
     <section id="contact" className="container pattern-dots-sm">
       <div className="header">
         <h1>Contact</h1>
-        <p>Get in touch</p>
+        <p>Let's build something great together!</p>
       </div>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <ul className="form-style">
-          {/* <p style={{ textAlign: "center", fontSize: "large" }}>Get in touch</p> */}
           <li>
             <label htmlFor="field1">
               Full Name <span className="required">*</span>
@@ -19,19 +60,33 @@ const Contact = () => {
               name="field1"
               className="field-divided"
               placeholder="First"
+              {...register("firstName", { required: true })}
             />{" "}
             <input
               type="text"
               name="field2"
               className="field-divided"
               placeholder="Last"
+              {...register("lastName", { required: true })}
             />
+            {errors.firstName?.type === "required" && "First name is required"}
+            {errors.lastName?.type === "required" && "Last name is required"}
           </li>
           <li>
             <label htmlFor="field3">
               Email <span className="required">*</span>
             </label>
-            <input type="email" name="field3" className="field-long" />
+            <input
+              type="email"
+              name="field3"
+              className="field-long"
+              {...register("email", {
+                required: true,
+                validate: validator.validate,
+              })}
+            />
+            {errors.email?.type === "required" && "Email is required"}
+            {errors.email?.type === "validate" && "Email is invalid"}
           </li>
           <li>
             <label htmlFor="field5">
@@ -41,10 +96,14 @@ const Contact = () => {
               name="field5"
               id="field5"
               className="field-long field-textarea"
+              {...register("messageBox", { required: true })}
             ></textarea>
+            {errors.messageBox?.type === "required" && "Message is required"}
           </li>
           <li>
-            <button className="btn">Send</button>
+            <button disabled={loading} className="btn">
+              Send
+            </button>
           </li>
         </ul>
       </form>
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import { ToastContainer } from "react-toastify";
 
 const Layout = (props) => {
   return (
     <>
       <Navbar />
       {props.children}
+      <ToastContainer position="top-center" autoClose={5000} />
       <Footer />
     </>
   );
