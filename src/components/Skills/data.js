7efc5d9605e0dd5966937a707134a2c8624c37c8
Ashwani Kumar Singh <code.ak.singh@gmail.com>
2024-01-15 15:53:40 +0530

    update: skills and about

--- src/components/Skills/data.js
+++ src/components/Skills/data.js
@@ -1,40 +1,60 @@
-import { FaGitAlt, FaLinux, FaReact, FaCss3, FaBootstrap } from "react-icons/fa"
-import { SiVisualstudiocode, SiPython, SiJava, SiC, SiJavascript, SiRedux, SiJquery, SiMaterialui, SiNodedotjs, SiExpress, SiMongodb } from "react-icons/si"
-import { AiFillHtml5 } from "react-icons/ai"
-import { BsGithub } from "react-icons/bs"
+import { FaGitAlt, FaLinux, FaReact, FaCss3 } from "react-icons/fa";
+import {
+  SiVisualstudiocode,
+  SiPython,
+  SiC,
+  SiJavascript,
+  SiNodedotjs,
+  SiExpress,
+  SiMongodb,
+  SiTailwindcss,
+  SiNextdotjs,
+  SiReactquery,
+  SiTypescript,
+} from "react-icons/si";
+import { AiFillHtml5 } from "react-icons/ai";
+import { BsGithub } from "react-icons/bs";
 
 const skills = [
-    {
-        type: "frontend", skills: [
-            { name: "HTML", icon: AiFillHtml5 },
-            { name: "CSS", icon: FaCss3 },
-            { name: "JavaScript", icon: SiJavascript },
-            { name: "React", icon: FaReact },
-            { name: "Bootstrap", icon: FaBootstrap },
-            { name: "Redux", icon: SiRedux },
-            { name: "jQuery", icon: SiJquery },
-            { name: "MaterialUI", icon: SiMaterialui }]
-    },
+  {
+    type: "frontend",
+    skills: [
+      {
+        name: "TypeScript",
+        icon: SiTypescript,
+      },
+      { name: "JavaScript", icon: SiJavascript },
+      { name: "Next.js", icon: SiNextdotjs },
+      { name: "React", icon: FaReact },
+      { name: "React Query", icon: SiReactquery },
+      { name: "Tailwind", icon: SiTailwindcss },
+      { name: "CSS", icon: FaCss3 },
+      { name: "HTML", icon: AiFillHtml5 },
+      // { name: "MaterialUI", icon: SiMaterialui }
+    ],
+  },
 
-    {
-        type: "backend", skills: [
-            { name: "Node", icon: SiNodedotjs },
-            { name: "Express", icon: SiExpress },
-            { name: "MongoDB", icon: SiMongodb },
-            { name: "C", icon: SiC },
-            { name: "Java", icon: SiJava },
-            { name: "Python", icon: SiPython }
-        ]
-    },
+  {
+    type: "backend",
+    skills: [
+      { name: "Node", icon: SiNodedotjs },
+      { name: "Express", icon: SiExpress },
+      { name: "MongoDB", icon: SiMongodb },
+      { name: "C", icon: SiC },
+      // { name: "Java", icon: SiJava },
+      { name: "Python", icon: SiPython },
+    ],
+  },
 
-    {
-        type: "tools", skills: [
-            { name: "Git", icon: FaGitAlt },
-            { name: "Github", icon: BsGithub },
-            { name: "VSCode", icon: SiVisualstudiocode },
-            { name: "Linux", icon: FaLinux }
-        ]
-    }
+  {
+    type: "tools",
+    skills: [
+      { name: "Git", icon: FaGitAlt },
+      { name: "Github", icon: BsGithub },
+      { name: "VSCode", icon: SiVisualstudiocode },
+      { name: "Linux", icon: FaLinux },
+    ],
+  },
 ];
 
-export default skills
+export default skills;
